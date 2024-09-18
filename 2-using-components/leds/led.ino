87a2f6fe36aa44fde1ee3fe7750023a50e544324
chiachenglu <82047138+chiachenglu@users.noreply.github.com>
2024-09-18 15:54:16 +0200

    Update led.ino
    back to previous version

--- 2-using-components/leds/led.ino
+++ 2-using-components/leds/led.ino
@@ -2,10 +2,13 @@
  * estape11 2024
  */
 
+#define LED_PIN 5
 
 // Runs once at power-on
 void setup() {
+  Serial.begin(115200);
   pinMode(LED_PIN, OUTPUT); // Tells the microcontroller how the pin is going to be used
+  digitalWrite(LED_PIN, HIGH);
 }
 
 // Runs indefitelely until power-off
