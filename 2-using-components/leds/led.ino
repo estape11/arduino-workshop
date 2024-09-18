fcfb12d11ebf6ff82b5b62d1c0c595e9189fb6aa
chiachenglu <82047138+chiachenglu@users.noreply.github.com>
2024-09-18 15:01:09 +0200

    Update led.ino
    changed back so people can change themselves.

--- 2-using-components/leds/led.ino
+++ 2-using-components/leds/led.ino
@@ -5,13 +5,13 @@
 
 // Runs once at power-on
 void setup() {
-  pinMode(13, OUTPUT); // Tells the microcontroller how the pin is going to be used
+  pinMode(LED_PIN, OUTPUT); // Tells the microcontroller how the pin is going to be used
 }
 
 // Runs indefitelely until power-off
 void loop() {
   delay(1000);
-  digitalWrite(13, LOW); // Sets a HIGH (5V) signal in the pin
+  digitalWrite(LED_PIN, LOW); // Sets a HIGH (5V) signal in the pin
   delay(1000);
-  digitalWrite(13, HIGH); // Sets a LOW (0V/GND) signal in the pin
+  digitalWrite(LED_PIN, HIGH); // Sets a LOW (0V/GND) signal in the pin
 }
